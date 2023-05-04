f6630b910048c57b358326613d6bc02bdfab7678
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-05-04 21:20:28 +0900

    指定したPORT番号でサーバーが立ち上がるかを確認してみよう

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+// http://localhost:8000/
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中・・・");
