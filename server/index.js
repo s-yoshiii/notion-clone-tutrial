deae030f50bb83029b8319aadf9efd08e1cecc3d
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-05-05 21:50:22 +0900

    サーバーとデータベースを接続、dotenvモジュールを使ってURLを保護

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 8000;
+require("dotenv").config();
+// DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("DBと接続中");
+} catch (error) {
+  console.log(error);
+}
+
 // http://localhost:8000/
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
 
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中・・・");
