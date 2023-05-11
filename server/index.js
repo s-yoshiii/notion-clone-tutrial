691967b30a3dfc818f182a5b9962d2fcec837f1d
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-05-12 01:03:08 +0900

    運用・保守しやすくするためにリファクタリングしよう：その１

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const CryptoJS = require("crypto-js");
-const JWT = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator");
-const User = require("./src/v1/models/user");
+
 const app = express();
 const PORT = 5000;
 require("dotenv").config();
 app.use(express.json());
+app.use("/api/v1", require("./src/v1/routes/auth"));
 // DB接続
 try {
   mongoose.connect(process.env.MONGODB_URL);
@@ -15,55 +13,6 @@ try {
 } catch (error) {
   console.log(error);
 }
-
-// ユーザー新規登録API
-app.post(
-  "/resister",
-  body("username")
-    .isLength({ min: 8 })
-    .withMessage("ユーザー名は8文字以上です。"),
-  body("password")
-    .isLength({ min: 8 })
-    .withMessage("パスワードは8文字以上です。"),
-  body("confirmPassword")
-    .isLength({ min: 8 })
-    .withMessage("確認用パスワードは8文字以上です。"),
-  body("username").custom((value) => {
-    return User.findOne({ username: value }).then((user) => {
-      if (user) {
-        return Promise.reject("このユーザーはすでに使われています。");
-      }
-    });
-  }),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
-  async (req, res) => {
-    // パスワードの受け取り
-    const password = req.body.password;
-
-    try {
-      // パスワードの暗号化
-      req.body.password = CryptoJS.AES.encrypt(
-        password,
-        process.env.SECRET_KEY
-      );
-      // ユーザーの新規作成
-      const user = await User.create(req.body);
-      // JWTの発行
-      const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
-        expiresIn: "24h",
-      });
-      return res.status(200).json({ user, token });
-    } catch (err) {
-      return res.status(500).json(err);
-    }
-  }
-);
 
 // http://localhost:8000/
 
