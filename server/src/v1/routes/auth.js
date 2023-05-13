95ac8b82dedd96e18fed1599fa37eccb2adecf5c
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-05-14 00:21:53 +0900

    運用・保守しやすくするためにリファクタリングしよう：その2

--- server/src/v1/routes/auth.js
+++ server/src/v1/routes/auth.js
@@ -1,9 +1,10 @@
 const { body, validationResult } = require("express-validator");
-const User = require("../models/user");
 const router = require("express").Router();
-const CryptoJS = require("crypto-js");
-const JWT = require("jsonwebtoken");
 require("dotenv").config();
+
+const User = require("../models/user");
+const validation = require("../handlers/validation");
+const userController = require("../controllers/users");
 // ユーザー新規登録API
 router.post(
   "/resister",
@@ -23,34 +24,8 @@ router.post(
       }
     });
   }),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
-  async (req, res) => {
-    // パスワードの受け取り
-    const password = req.body.password;
-
-    try {
-      // パスワードの暗号化
-      req.body.password = CryptoJS.AES.encrypt(
-        password,
-        process.env.SECRET_KEY
-      );
-      // ユーザーの新規作成
-      const user = await User.create(req.body);
-      // JWTの発行
-      const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
-        expiresIn: "24h",
-      });
-      return res.status(200).json({ user, token });
-    } catch (err) {
-      return res.status(500).json(err);
-    }
-  }
+  validation.validate,
+  userController.resister
 );
 
 module.exports = router;
