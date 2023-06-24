2df6c47b5ee2a8cdf05853304f3ab650d77e9948
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-06-25 00:29:51 +0900

    認証に関する共通ページの構築を始めよう

--- client/src/components/layout/authLayout.jsx
+++ client/src/components/layout/authLayout.jsx
@@ -1,11 +1,34 @@
+import { Box, Container } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import logo from "../../assets/imges/notion-logo.png";
 
 const authLayout = () => {
   return (
     <div>
-      authLayout
-      <Outlet />
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 6,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            src={logo}
+            style={{
+              width: 100,
+              height: 100,
+              marginBottom: 3,
+            }}
+            alt=""
+          />
+          Notionクローン開発
+        </Box>
+        authLayout
+        <Outlet />
+      </Container>
     </div>
   );
 };
