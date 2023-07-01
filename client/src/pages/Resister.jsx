425c1c0a117a01f5d89588ff4823c502796c4ffc
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-07-02 00:28:22 +0900

    送信ボタンとログイン遷移用のボタンを作成しよう

--- client/src/pages/Resister.jsx
+++ client/src/pages/Resister.jsx
@@ -1,36 +1,53 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import React from "react";
+import { LoadingButton } from "@mui/lab";
+import { Link } from "react-router-dom";
 
 const Resister = () => {
   return (
-    <Box component="form">
-      <TextField
-        fullWidth
-        id="username"
-        name="username"
-        label="お名前"
-        margin="normal"
-        required
-      />
-      <TextField
-        fullWidth
-        id="password"
-        name="password"
-        label="パスワード"
-        margin="normal"
-        type="password"
-        required
-      />
-      <TextField
-        fullWidth
-        id="confirmPassword"
-        name="confirmPassword"
-        label="確認用パスワード"
-        margin="normal"
-        type="password"
-        required
-      />
-    </Box>
+    <>
+      <Box component="form">
+        <TextField
+          fullWidth
+          id="username"
+          name="username"
+          label="お名前"
+          margin="normal"
+          required
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="パスワード"
+          margin="normal"
+          type="password"
+          required
+        />
+        <TextField
+          fullWidth
+          id="confirmPassword"
+          name="confirmPassword"
+          label="確認用パスワード"
+          margin="normal"
+          type="password"
+          required
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          fullWidth
+          type="submit"
+          loading={false}
+          color="primary"
+          variant="outlined"
+        >
+          アカウント作成
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login">
+        すでにアカウントを持っていますか？ログイン
+      </Button>
+    </>
   );
 };
 
